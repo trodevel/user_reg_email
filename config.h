8ff54c0991b797f57e739628fe1c65e59571bc28
trodevel <trodevel@gmail.com>
2020-09-26 08:10:04 +0200

    extracted Config

---
+++ config.h
@@ -0,0 +1,47 @@
+/*
+
+User Reg via Email.
+
+Copyright (C) 2019 Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// $Revision: 13839 $ $Date:: 2020-09-25 #$ $Author: serge $
+
+#ifndef USER_REG__CONFIG_H
+#define USER_REG__CONFIG_H
+
+#include <string>               // std::unique_ptr
+
+namespace user_reg_email
+{
+
+struct Config
+{
+    std::string     host_name;
+    uint32_t        port;
+    std::string     username;
+    std::string     password;
+    std::string     sender_email;
+    std::string     sender_name;
+    std::string     subject;
+    std::string     body_template_filename;
+};
+
+} // namespace user_reg_email
+
+
+#endif // USER_REG__CONFIG_H
--- user_reg_email.h
+++ user_reg_email.h
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-// $Revision: 13828 $ $Date:: 2020-09-23 #$ $Author: serge $
+// $Revision: 13839 $ $Date:: 2020-09-25 #$ $Author: serge $
 
 #ifndef USER_REG__USER_REG_EMAIL_H
 #define USER_REG__USER_REG_EMAIL_H
@@ -29,24 +29,13 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 #include "user_reg/user_reg.h"  // UserReg
 #include "utils/EMailSender.h"  // utils::EMailSender
 #include "templtext/templ.h"    // templtext::Templ
+#include "config.h"             // Config
 
 namespace user_reg_email
 {
 
 using user_id_t = user_reg::user_id_t;
 using group_id_t = user_reg::group_id_t;
-
-struct Config
-{
-    std::string     host_name;
-    uint32_t        port;
-    std::string     username;
-    std::string     password;
-    std::string     sender_email;
-    std::string     sender_name;
-    std::string     subject;
-    std::string     body_template_filename;
-};
 
 class UserRegEmail
 {
